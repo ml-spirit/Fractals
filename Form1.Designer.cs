50fc18784c98d5db42058e17e4b88674dd9d5574
ml-spirit <masha.lozinska2007@gmail.com>
2022-12-23 21:10:30 +0200

    Первая версия

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,7 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,29 +13,84 @@
         }
 
         #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.refresh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.offsetX = new System.Windows.Forms.TextBox();
+            this.offsetY = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(53, 438);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(116, 38);
+            this.refresh.TabIndex = 0;
+            this.refresh.Text = "Обновить";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 383);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Смещение Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Смещение X";
+            // 
+            // offsetX
+            // 
+            this.offsetX.Location = new System.Drawing.Point(53, 354);
+            this.offsetX.Name = "offsetX";
+            this.offsetX.Size = new System.Drawing.Size(116, 26);
+            this.offsetX.TabIndex = 3;
+            this.offsetX.Text = "40";
+            // 
+            // offsetY
+            // 
+            this.offsetY.Location = new System.Drawing.Point(53, 406);
+            this.offsetY.Name = "offsetY";
+            this.offsetY.Size = new System.Drawing.Size(116, 26);
+            this.offsetY.TabIndex = 4;
+            this.offsetY.Text = "40";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(1176, 709);
+            this.Controls.Add(this.offsetY);
+            this.Controls.Add(this.offsetX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.refresh);
             this.Name = "Form1";
             this.Text = "Fractals";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox offsetX;
+        private System.Windows.Forms.TextBox offsetY;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Fractals
 {
     public partial class Form1 : Form
     {
+        private Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void DrawCircle(Graphics graphics, int x, int y, int radius, Color color)
         {
+            graphics.DrawEllipse(new Pen(color), x, y, radius, radius);
+            graphics.FillEllipse(new SolidBrush(color), x, y, radius, radius);
+        }
 
+        private void DrawRect(Graphics graphics, int x, int y, int width, int height, Color color)
+        {
+            graphics.DrawRectangle(new Pen(color), x, y, width, height);
+            graphics.FillRectangle(new SolidBrush(color), x, y, width, height);
+        }
+
+        private void DrawTriangle(Graphics graphics, int x1, int y1, int x2, int y2, int x3, int y3,  Color color)
+        {
+            var context = new Point[3];
+            context[0].X = x1;
+            context[0].Y = y1;
+            context[1].X = x2;
+            context[1].Y = y2;
+            context[2].X = x3;
+            context[2].Y = y3;
+            graphics.DrawPolygon(new Pen(color), context);
+            graphics.FillPolygon(new SolidBrush(color), context);
+        }
+
+        private void DrawObject(Graphics graphics, int x, int y, int offsetX, int offsetY, int deltaX, int deltaY, int radius, Color color)
+        {
+            DrawCircle(graphics, x + offsetX * deltaX, y + offsetY * deltaY, radius, color);
+            //DrawRect(graphics, x + offsetX * deltaX, y + offsetY * deltaY, radius, radius, color);
+        }
+
+        private void DrawObjects(Graphics graphics, int x, int y, int offsetX, int offsetY, Color color, int deep)
+        {
+            var radius = rand.Next(5, 30);
+            {
+                DrawObject(graphics, x, y, offsetX, offsetY, 1, 1, radius, color);
+                DrawObject(graphics, x, y, offsetX, offsetY, 1, -1, radius, color);
+                DrawObject(graphics, x, y, offsetX, offsetY, -1, 1, radius, color);
+                DrawObject(graphics, x, y, offsetX, offsetY, -1, -1, radius, color);
+            }
+            if (deep < 10)
+            {
+                DrawObjects(
+                    graphics,
+                    x,
+                    y,
+                    offsetX + rand.Next(-10, 30),
+                    offsetY + rand.Next(-10, 30),
+                    color,
+                    deep + 1);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var _offsetX = int.Parse(this.offsetX.Text);
+            var _offsetY = int.Parse(this.offsetY.Text);
+            {
+                DrawObjects(e.Graphics, ClientSize.Width / 2, ClientSize.Height / 2, _offsetX, _offsetY, Color.FromArgb(70, 0, 0, rand.Next(0, 200)), 0);
+                DrawObjects(e.Graphics, ClientSize.Width / 2, ClientSize.Height / 2, _offsetX, _offsetY, Color.FromArgb(70, 0, rand.Next(0, 200), 0), 0);
+                DrawObjects(e.Graphics, ClientSize.Width / 2, ClientSize.Height / 2, _offsetX, _offsetY, Color.FromArgb(70, rand.Next(0, 200), 0, 0), 0);
+                DrawObjects(e.Graphics, ClientSize.Width / 2, ClientSize.Height / 2, _offsetX, _offsetY, Color.FromArgb(70, rand.Next(0, 200), 0, 255), 0);
+            }
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
         }
     }
 }
