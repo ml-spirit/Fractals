1f5ee457708dc693d0884c753ec02bb6a26a9938
ml-spirit <masha.lozinska2007@gmail.com>
2022-12-24 21:10:09 +0200

    check my 1 project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -20,13 +20,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.offsetX = new System.Windows.Forms.TextBox();
             this.offsetY = new System.Windows.Forms.TextBox();
+            this.radius = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.shape = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // refresh
             // 
-            this.refresh.Location = new System.Drawing.Point(53, 438);
+            this.refresh.Location = new System.Drawing.Point(35, 285);
+            this.refresh.Margin = new System.Windows.Forms.Padding(2);
             this.refresh.Name = "refresh";
-            this.refresh.Size = new System.Drawing.Size(116, 38);
+            this.refresh.Size = new System.Drawing.Size(77, 25);
             this.refresh.TabIndex = 0;
             this.refresh.Text = "Обновить";
             this.refresh.UseVisualStyleBackColor = true;
@@ -35,47 +40,95 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 383);
+            this.label1.Location = new System.Drawing.Point(33, 249);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Смещение Y";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 331);
+            this.label2.Location = new System.Drawing.Point(33, 215);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Смещение X";
             // 
             // offsetX
             // 
-            this.offsetX.Location = new System.Drawing.Point(53, 354);
+            this.offsetX.Location = new System.Drawing.Point(35, 230);
+            this.offsetX.Margin = new System.Windows.Forms.Padding(2);
             this.offsetX.Name = "offsetX";
-            this.offsetX.Size = new System.Drawing.Size(116, 26);
+            this.offsetX.Size = new System.Drawing.Size(79, 20);
             this.offsetX.TabIndex = 3;
             this.offsetX.Text = "40";
             // 
             // offsetY
             // 
-            this.offsetY.Location = new System.Drawing.Point(53, 406);
+            this.offsetY.Location = new System.Drawing.Point(35, 264);
+            this.offsetY.Margin = new System.Windows.Forms.Padding(2);
             this.offsetY.Name = "offsetY";
-            this.offsetY.Size = new System.Drawing.Size(116, 26);
+            this.offsetY.Size = new System.Drawing.Size(79, 20);
             this.offsetY.TabIndex = 4;
             this.offsetY.Text = "40";
             // 
+            // radius
+            // 
+            this.radius.Location = new System.Drawing.Point(35, 192);
+            this.radius.Name = "radius";
+            this.radius.Size = new System.Drawing.Size(79, 20);
+            this.radius.TabIndex = 5;
+            this.radius.Text = "40";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Радиус";
+            // 
+            // shape
+            // 
+            this.shape.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.shape.FormattingEnabled = true;
+            this.shape.Items.AddRange(new object[] {
+            "Круг",
+            "Треугольник",
+            "Квадрат"});
+            this.shape.Location = new System.Drawing.Point(36, 152);
+            this.shape.Name = "shape";
+            this.shape.Size = new System.Drawing.Size(78, 21);
+            this.shape.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Выбор фигуры";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1176, 709);
+            this.ClientSize = new System.Drawing.Size(784, 456);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.shape);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radius);
             this.Controls.Add(this.offsetY);
             this.Controls.Add(this.offsetX);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.refresh);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Fractals";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -91,6 +144,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox offsetX;
         private System.Windows.Forms.TextBox offsetY;
+        private System.Windows.Forms.TextBox radius;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox shape;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,27 +4,34 @@ using System.Windows.Forms;
 
 namespace Fractals
 {
+    //головна форма программи 
     public partial class Form1 : Form
     {
+        //головний генератор рандомних чисел
         private Random rand = new Random();
 
+        //ініціалізація усієї программи 
         public Form1()
         {
             InitializeComponent();
+            shape.SelectedIndex = 0;
         }
 
+        //функція яка малює коло
         private void DrawCircle(Graphics graphics, int x, int y, int radius, Color color)
         {
             graphics.DrawEllipse(new Pen(color), x, y, radius, radius);
             graphics.FillEllipse(new SolidBrush(color), x, y, radius, radius);
         }
 
+        //функція яка малює прямокутник
         private void DrawRect(Graphics graphics, int x, int y, int width, int height, Color color)
         {
             graphics.DrawRectangle(new Pen(color), x, y, width, height);
             graphics.FillRectangle(new SolidBrush(color), x, y, width, height);
         }
 
+        //функція яка малює трикутник
         private void DrawTriangle(Graphics graphics, int x1, int y1, int x2, int y2, int x3, int y3,  Color color)
         {
             var context = new Point[3];
@@ -38,15 +45,40 @@ namespace Fractals
             graphics.FillPolygon(new SolidBrush(color), context);
         }
 
+        //функція яка малює фігуру в залежності від вибору користувача 
         private void DrawObject(Graphics graphics, int x, int y, int offsetX, int offsetY, int deltaX, int deltaY, int radius, Color color)
         {
-            DrawCircle(graphics, x + offsetX * deltaX, y + offsetY * deltaY, radius, color);
-            //DrawRect(graphics, x + offsetX * deltaX, y + offsetY * deltaY, radius, radius, color);
+            if (shape.SelectedIndex == 0)
+            {
+                DrawCircle(graphics, x + offsetX * deltaX, y + offsetY * deltaY, radius, color);
+                return;
+            }
+            if (shape.SelectedIndex == 2)
+            { 
+                DrawRect(graphics, x + offsetX * deltaX, y + offsetY * deltaY, radius, radius, color);
+                return;  
+            }
+            if (shape.SelectedIndex == 1) 
+            {
+                DrawTriangle(
+                    graphics,
+                    x + offsetX * deltaX,
+                    y + offsetY * deltaY,
+                    x + offsetX * deltaX + radius,
+                    y + offsetY * deltaY + radius,
+                    x + offsetX * deltaX + radius * 5,
+                    y + offsetY * deltaY + radius * 3,
+                    color);
+            }
         }
 
+        //функція малює симетрію за віссю ох та оу
         private void DrawObjects(Graphics graphics, int x, int y, int offsetX, int offsetY, Color color, int deep)
         {
-            var radius = rand.Next(5, 30);
+            var value = int.Parse(this.radius.Text);
+            if (value <= 5)
+                value = 5;
+            var radius = rand.Next(5, value);
             {
                 DrawObject(graphics, x, y, offsetX, offsetY, 1, 1, radius, color);
                 DrawObject(graphics, x, y, offsetX, offsetY, 1, -1, radius, color);
@@ -66,6 +98,7 @@ namespace Fractals
             }
         }
 
+        //функція яка малює все
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             var _offsetX = int.Parse(this.offsetX.Text);
@@ -78,6 +111,7 @@ namespace Fractals
             }
         }
 
+        //функція яка генерує нову картинку 
         private void refresh_Click(object sender, EventArgs e)
         {
             this.Refresh();
